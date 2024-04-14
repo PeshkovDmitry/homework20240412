e4623ad6e06acd0a909af2ee712fbb5b5f8cd43f
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-04-14 10:34:28 +0300

    Initial commit

---
+++ src/main/java/ru/gb/homework20240412/Homework20240412Application.java
@@ -0,0 +1,13 @@
+package ru.gb.homework20240412;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework20240412Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework20240412Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/homework20240412/Homework20240412ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.homework20240412;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework20240412ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
